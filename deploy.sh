a1822b23cdfbb2c6e12df3d103b19037bd6047e3
Chen9818 <88109684+Chen9818@users.noreply.github.com>
2022-03-07 01:40:21 +0800

    init

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Chen9818/vue-week6.git master:gh-pages
+git push -f git@github.com:Chen9818/vue-week7.git master:gh-pages
 
 cd -
